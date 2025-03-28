96d5f756c28e512ef700076c652b2f772b446287
amareshka <amareshbevoor@gmail.com>
2025-03-28 22:51:47 +0530

    Fetching products and displaying

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,44 @@
-import './App.css'
+import "./App.css";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
+  const [productsList, setProductsList] = useState([]);
 
+  const fetchProducts = async () => {
+    const res = await fetch("https://dummyjson.com/products");
+    const data = await res.json();
+
+    if (data && data.products) {
+      setProductsList(data.products);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
   return (
     <>
-      <h1>Welcome to pagination</h1>
+      {productsList.length > 0 && (
+        <div className="products">
+          {productsList.map((prod) => {
+            return (
+              <div className="product" key={prod.id}>
+                <img src={prod.thumbnail} alt={prod.title} />
+                <span>{prod.title}</span>
+              </div>
+            );
+          })}
+        </div>
+      )}
+      <div className="paginationContainer">
+        <span>⬅️</span>
+        <span>1</span>
+        <span>➡️</span>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
